dca540bd21475dd2c388524de8806784ad6e11ae
Sammy Junior <samjrdev98@gmail.com>
2026-06-30 16:26:05 -0300

    chore(retrieval): export PathRetriever and MultiAnchorRetriever

--- glyph/retrieval/__init__.py
+++ glyph/retrieval/__init__.py
@@ -1,1 +1,16 @@
 """Graph-aware retrieval and hybrid fusion."""
+from glyph.retrieval.community import CommunityRetriever
+from glyph.retrieval.graph import GraphRetriever
+from glyph.retrieval.hybrid import HybridRetriever
+from glyph.retrieval.multi_anchor import MultiAnchorRetriever
+from glyph.retrieval.path import PathRetriever
+from glyph.retrieval.port import Retriever
+
+__all__ = [
+    "CommunityRetriever",
+    "GraphRetriever",
+    "HybridRetriever",
+    "MultiAnchorRetriever",
+    "PathRetriever",
+    "Retriever",
+]
